db81f09aa7bcdd0b414219bd860b50cef76f9b38
lai2-24 <lai2-24@itismeucci.com>
2023-10-31 14:36:45 +0100

    fine primo giorno, client da fare, main server da fare, ServerThread da finire il run

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,35 @@
 package com.example;
 
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    try{
+            // metto il server in ascolto sulla porta 3000 per poter acquisire e creare la socket
+        ServerSocket server = new ServerSocket(3000);
+        System.out.println("il server è in ascolto");
+        System.out.println("inserisci lo username da usare");
+        Scanner cin = new Scanner(System.in);
+        String nome = cin.nextLine();
+        HashMap<String,ServerThread> partecipanti = new HashMap();
+
+        while(true){
+
+            ServerThread p = new ServerThread(server.accept(),partecipanti);
+            partecipanti.put(nome, p);
+            p.start();
+
+        }
+
+    } catch (Exception e) {
+            System.out.println(e.getMessage());
     }
 }
+
+
---
+++ src/main/java/com/example/ServerThread.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class ServerThread extends Thread{
+    private Socket socket;
+    private HashMap <String, ServerThread> partecipanti;
+    BufferedReader in;
+    DataOutputStream out;
+
+    public ServerThread(Socket socket, HashMap partecipanti){
+        this.socket = socket;
+        this.partecipanti = partecipanti;
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//input
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());//output
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void run(){
+        try{
+            System.out.println("----------connected to the chat----------");
+            in.readLine();
+            while(true){
+                
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+       
+    }
+
+    private void broadcast(String message) throws IOException{
+        for(String name: partecipanti.keySet()){
+            if(currentThread() != partecipanti.get(name))
+                unicast(message, name);
+        }
+    }
+    
+    private void unicast(String message, String name) throws IOException{
+        partecipanti.get(name).send(message);
+    }
+
+
+    private void send(String message) throws IOException{
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());//output
+        out.writeBytes(message);
+    }
+    
+
+}
